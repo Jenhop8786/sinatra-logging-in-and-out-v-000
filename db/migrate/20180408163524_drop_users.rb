90b5c502f6f7e3b477000c66f0f4ac33ef88bb6a
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-04-08 16:39:20 +0000

    automatically backed up by learn

--- db/migrate/20180408163524_drop_users.rb
+++
@@ -1,9 +0,0 @@
-class DropUsers < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :users do |t|
-      t.string :username
-      t.string :password
-      t.float :balance
-    end 
-  end
-end
